989d6381190a4f05d46c30a7ced6b2383ffc1952
GodHedgehog <iezov298@gmail.com>
2025-08-23 19:10:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,24 @@ from jnius import autoclass
 
 class TestApp(App):
     def build(self):
-        return Button(text="Показать 'ошибку'", on_press=self.show_error)
+        return Button(text="Показать современный диалог", on_press=self.show_error)
 
     def show_error(self, *args):
         PythonActivity = autoclass('org.kivy.android.PythonActivity')
-        AlertDialogBuilder = autoclass('android.app.AlertDialog$Builder')
+        MaterialAlertDialogBuilder = autoclass('com.google.android.material.dialog.MaterialAlertDialogBuilder')
         JavaString = autoclass('java.lang.String')
 
         activity = PythonActivity.mActivity
 
         def make_dialog():
-            dialog = AlertDialogBuilder(activity)
+            dialog = MaterialAlertDialogBuilder(activity)
             dialog.setTitle(JavaString("Ошибка"))
             dialog.setMessage(JavaString("К сожалению, приложение остановлено."))
             dialog.setCancelable(False)
             dialog.setPositiveButton(JavaString("OK"), None)
+            dialog.setNegativeButton(JavaString("Отправить отчёт"), None)
             dialog.show()
 
-        # ВАЖНО: выполняем в UI-потоке Android
         activity.runOnUiThread(make_dialog)
 
 TestApp().run()
