f78a58b5f6f81da915a0ba7b20611c2cac000e7f
GodHedgehog <iezov298@gmail.com>
2025-08-23 18:57:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from jnius import autoclass, cast
+from jnius import autoclass
 
 class TestApp(App):
     def build(self):
         return Button(text="Показать 'ошибку'", on_press=self.show_error)
 
     def show_error(self, *args):
-        # Android классы
         PythonActivity = autoclass('org.kivy.android.PythonActivity')
         AlertDialogBuilder = autoclass('android.app.AlertDialog$Builder')
-        DialogInterface = autoclass('android.content.DialogInterface')
         JavaString = autoclass('java.lang.String')
 
         activity = PythonActivity.mActivity
-        dialog = AlertDialogBuilder(activity)
 
-        # ВАЖНО: оборачиваем строки в java.lang.String
-        dialog.setTitle(JavaString("Ошибка"))
-        dialog.setMessage(JavaString("К сожалению, приложение остановлено."))
-        dialog.setCancelable(False)
+        def make_dialog():
+            dialog = AlertDialogBuilder(activity)
+            dialog.setTitle(JavaString("Ошибка"))
+            dialog.setMessage(JavaString("К сожалению, приложение остановлено."))
+            dialog.setCancelable(False)
+            dialog.setPositiveButton(JavaString("OK"), None)
+            dialog.show()
 
-        def on_click(dialog_interface, which):
-            dialog_interface.dismiss()
-
-        dialog.setPositiveButton(JavaString("OK"), on_click)
-        dialog.show()
+        # ВАЖНО: выполняем в UI-потоке Android
+        activity.runOnUiThread(make_dialog)
 
 TestApp().run()
