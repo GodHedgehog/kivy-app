aa40da0fce2fc5d9a9f2f3a5d8eda54f6aba31b2
GodHedgehog <iezov298@gmail.com>
2025-08-24 11:46:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ class TestApp(App):
     def show_toast(self, *args):
         PythonActivity = autoclass('org.kivy.android.PythonActivity')
         Toast = autoclass('android.widget.Toast')
+        String = autoclass('java.lang.String')
 
         activity = PythonActivity.mActivity
 
         def toast_message():
-            Toast.makeText(activity, "Привет из Python!", Toast.LENGTH_SHORT).show()
+            message = String("Привет из Python!")
+            Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
 
-        # обязательно в UI-потоке
+        # Важно: запускать в UI-потоке
         activity.runOnUiThread(toast_message)
 
 TestApp().run()
