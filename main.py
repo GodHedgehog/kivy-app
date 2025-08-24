56efc0b20741fc82b8acdc5fe92dbd200c81d5e7
GodHedgehog <iezov298@gmail.com>
2025-08-24 11:33:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+from kivy.app import App
+from kivy.uix.button import Button
 from jnius import autoclass
 
-PythonActivity = autoclass('org.kivy.android.PythonActivity')
-Toast = autoclass('android.widget.Toast')
+class TestApp(App):
+    def build(self):
+        return Button(text="Показать Toast", on_press=self.show_toast)
 
-activity = PythonActivity.mActivity
-Toast.makeText(activity, "Привет из Python!", Toast.LENGTH_SHORT).show()
+    def show_toast(self, *args):
+        PythonActivity = autoclass('org.kivy.android.PythonActivity')
+        Toast = autoclass('android.widget.Toast')
+
+        activity = PythonActivity.mActivity
+
+        def toast_message():
+            Toast.makeText(activity, "Привет из Python!", Toast.LENGTH_SHORT).show()
+
+        # обязательно в UI-потоке
+        activity.runOnUiThread(toast_message)
+
+TestApp().run()
