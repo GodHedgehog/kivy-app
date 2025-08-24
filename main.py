08b6a2fa887a8a8d6524af389b1dbcfc58e32b70
GodHedgehog <iezov298@gmail.com>
2025-08-24 11:24:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-from kivy.app import App
-from kivy.uix.button import Button
 from jnius import autoclass
 
-class TestApp(App):
-    def build(self):
-        return Button(text="Показать современный диалог", on_press=self.show_error)
+PythonActivity = autoclass('org.kivy.android.PythonActivity')
+Toast = autoclass('android.widget.Toast')
 
-    def show_error(self, *args):
-        PythonActivity = autoclass('org.kivy.android.PythonActivity')
-        MaterialAlertDialogBuilder = autoclass('com.google.android.material.dialog.MaterialAlertDialogBuilder')
-        JavaString = autoclass('java.lang.String')
-
-        activity = PythonActivity.mActivity
-
-        def make_dialog():
-            dialog = MaterialAlertDialogBuilder(activity)
-            dialog.setTitle(JavaString("Ошибка"))
-            dialog.setMessage(JavaString("К сожалению, приложение остановлено."))
-            dialog.setCancelable(False)
-            dialog.setPositiveButton(JavaString("OK"), None)
-            dialog.setNegativeButton(JavaString("Отправить отчёт"), None)
-            dialog.show()
-
-        activity.runOnUiThread(make_dialog)
-
-TestApp().run()
+activity = PythonActivity.mActivity
+Toast.makeText(activity, "Привет из Python!", Toast.LENGTH_SHORT).show()
